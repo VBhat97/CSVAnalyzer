47b35b6989b005edc445389a6653e60d33b33590
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-08-12 03:52:15 -0400

    Passing form data to backend

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/form-action')
+def form_action():
+    return render_template('form_action.html', email=request.args['email'])
