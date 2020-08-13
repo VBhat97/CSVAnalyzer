f00d4bfec1fb61587caad6d908b580e0a7c7ee07
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-08-12 20:52:11 -0400

    Added file transfer working

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ def home():
 @app.route('/form-action', methods=["GET","POST"])
 def form_action():
     if request.method == 'POST':
-        return render_template('form_action.html', email=request.form['email'], file=request.form['myfile'])
-    else :
+        email=request.form['email']
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return render_template('form_action.html', email , file=request.form['myfile'])
+    else:
         return redirect('/')
