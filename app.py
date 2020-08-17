463209d0e93061c8a16b79aba4494490aef36934
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-08-16 21:59:57 -0400

    Changes in app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask,render_template,request,redirect
 from werkzeug import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'C:/Users/V.Bhat/Desktop/Graduate Studies/Co-Corriculars/Projects/CSVAnalyzer/uploads'
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
@@ -11,5 +13,7 @@ def home():
 @app.route('/form-action', methods=["GET","POST"])
 def form_action():
     if request.method == 'POST':
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     else:
         return redirect('/')
