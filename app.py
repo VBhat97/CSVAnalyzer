008d9f5314a7e71b29003512bf9311ecaab59689
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-08-17 19:25:17 -0400

    Working till here

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask,render_template,request,redirect
 from werkzeug import secure_filename
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'C:/Users/V.Bhat/Desktop/Graduate Studies/Co-Corriculars/Projects/CSVAnalyzer/uploads'
+# UPLOAD_FOLDER = 'C:/Users/V.Bhat/Desktop/Graduate Studies/Co-Corriculars/Projects/CSVAnalyzer/uploads'
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/form-action', methods=["GET","POST"])
+@app.route('/form_action', methods=["GET","POST"])
 def form_action():
     if request.method == 'POST':
-
+        return render_template('form_action.html')
     else:
         return redirect('/')
