acdd47cf825bf03971678cac156128e5445dbd59
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-08-19 19:32:50 -0400

    Changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,flash
 from werkzeug import secure_filename
 
 app = Flask(__name__)
@@ -11,9 +11,11 @@ def home():
 @app.route('/form_action', methods=["GET","POST"])
 def form_action():
     if request.method == 'POST':
-        filename='Hello'
-        # file = request.files['file']
-        # filename = secure_filename(file.filename)
+        if 'file' not in request.files:
+            flash('No file part')
+            return render_template('home.html')
+        file = request.files['file']
+        filename = secure_filename(file.filename)
         # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return render_template('form_action.html',filename=filename)
     else:
