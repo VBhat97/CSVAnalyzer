8bae099db9d1d5cf8fb362f004a379c7b59275a2
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-08-20 22:16:33 -0400

    Working file upload

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask,render_template,request,redirect,flash
 from werkzeug import secure_filename
 
 app = Flask(__name__)
-UPLOAD_FOLDER = '/uploads'
 
 @app.route('/')
 def home():
@@ -11,12 +10,9 @@ def home():
 @app.route('/form_action', methods=["GET","POST"])
 def form_action():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return render_template('home.html')
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template('form_action.html',filename=filename)
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(uploaded_file.filename)
+        return render_template('form_action.html',filename='Hello')
     else:
         return redirect('/')
