4c6b07293c36671e83360cea0b55781401830e65
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-09-07 23:11:06 -0400

    Done till accuracy of Linear SVC

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask,render_template,request,redirect,flash,session
 from werkzeug import secure_filename
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
 app = Flask(__name__)
 app.secret_key = "aLKG21BFAJH"
@@ -39,6 +43,8 @@ def results_action():
         data_features=data.iloc[:,int(traincols[0]):int(traincols[1])]
         data_output=data.iloc[:,int(testcol)]
         X_train, X_test, y_train, y_test = train_test_split(data_features, data_output, test_size=0.2, random_state=42)
-        print(len(X_test))
-        print(len(X_train))
+        clf = make_pipeline(StandardScaler(),LinearSVC(random_state=0, tol=1e-5))
+        clf.fit(X_train, y_train)
+        y_pred=clf.predict(X_test)
+        print(accuracy_score(y_test,y_pred))
     return render_template('results.html', traincols=traincols, testcol=testcol)
