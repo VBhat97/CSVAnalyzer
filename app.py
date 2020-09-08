baea4acf521a38e254e446d835dd144a5530d6ed
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-09-07 23:41:54 -0400

    Added TODOs and Linear SVC

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def results_action():
         data_features=data.iloc[:,int(traincols[0]):int(traincols[1])]
         data_output=data.iloc[:,int(testcol)]
         X_train, X_test, y_train, y_test = train_test_split(data_features, data_output, test_size=0.2, random_state=42)
-        # TODO: Just for Linear SVC. More to be added soon.
+        # TODO: Just for Linear SVC. More to be added.
         clf = make_pipeline(StandardScaler(),LinearSVC(random_state=0, tol=1e-5))
         clf.fit(X_train, y_train)
         y_pred=clf.predict(X_test)
